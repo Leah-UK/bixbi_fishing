e6ff4d51f819b38270fa0047a35d7ee12c6dac51
Leah <mail@leahuk.com>
2022-01-21 09:59:29 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,37 +1,5 @@
 Config = {}
 
-Config.Debug = true
+Config.Debug = true -- Allows live-restarting of the script.
 Config.FishTime = {13, 32} -- In seconds. Lowest and highest time to wait while fishing.
 Config.Fish = { 'pike', 'bream', 'zander', 'salmon', 'trout', 'cod', 'herring', 'tuna' }
-  --    [1] = { 
-  --       { itemName = 'pike', quantity = 1},
-  --    },
-     
-  --    [2] = { 
-  --      { itemName = 'bream', quantity = 1},
-  --    },
-   
-  --    [3] = {
-  --      { itemName = 'zander', quantity = 1},
-  --    },
-   
-  --    [4] = {
-  --      { itemName = 'salmon', quantity = 1},
-  --    },
-   
-  --    [5] = {
-  --      { itemName = 'trout', quantity = 1},
-  --    },
-   
-  --    [6] = {
-  --      { itemName = 'cod', quantity = 1},
-  --    },
-   
-  --    [7] = {
-  --      { itemName = 'herring', quantity = 1},
-  --    },
-   
-  --    [8] = {
-  --      { itemName = 'tuna', quantity = 1},
-  --    },
-  --  }
